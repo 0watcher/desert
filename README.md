705301c4510ab46f0cd7d590994160101bf252b0
Patryk Strużek <patrykstruzek17@gmail.com>
2024-03-19 10:44:57 +0100

    Should i add another struct for tables?

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # desert
- simple serde wrapper for rusqlite
+ Simple serde wrapper for rusqlite, that prevent you among others from harcoding queries. It isn't complete because I'am not handling all possible query cases available in SQL.
+
+# Cargo Add
+```cargo add desert``` 
+
+# Example
+```rust
+#[derive(Serialize)]
+struct Person {
+    id: u32,
+    name: String,
+    email: String,
+}
+
+Db::mem().insert_one(Person);
+```
--- src/db.rs
+++ src/db.rs
@@ -3,38 +3,47 @@ use std::path::Path;
 use rusqlite::{Connection, Result};
 use serde::{Deserialize, Serialize};
 
+pub enum SelectOpt {
+    All,
+    Distinct,
+    Where(String),
+    OrderBy(bool),
+}
+
+pub enum UpdateOpt {
+    Where(String),
+}
+
 struct Db {
     conn: Connection,
 }
 
 impl Db {
-    pub fn open(&mut self, path: &mut Path) -> Self {
-        Db {
-            conn: self::Connection::open(path).unwrap(),
-        }
+    pub fn open(path: &mut Path) -> Result<Self, rusqlite::Error> {
+        let conn = Connection::open(path)?;
+        Ok(Db { conn })
     }
 
-    pub fn mem(&mut self) -> Self {
-        Db {
-            conn: self::Connection::open_in_memory().unwrap(),
-        }
+    pub fn mem() -> Result<Self, rusqlite::Error> {
+        let conn = Connection::open_in_memory()?;
+        Ok(Db { conn })
     }
 
-    pub fn select_all<T: Serialize>() {}
+    pub fn create(&mut self) {}
 
-    pub fn select<T: Serialize>() {}
+    pub fn select<T: Serialize>(&mut self) {}
 
-    pub fn insert_one<'a, T: Deserialize<'a>>() {}
+    pub fn insert_one<'a, T: Deserialize<'a>>(&mut self) {}
 
-    pub fn insert_many<'a, T: Deserialize<'a>>() {}
+    pub fn insert_many<'a, T: Deserialize<'a>>(&mut self) {}
 
-    pub fn update_one<'a, T: Deserialize<'a>>() {}
+    pub fn update_one<'a, T: Deserialize<'a>>(&mut self) {}
 
-    pub fn update_many<'a, T: Deserialize<'a>>() {}
+    pub fn update_many<'a, T: Deserialize<'a>>(&mut self) {}
 
-    pub fn delete_one<'a, T: Deserialize<'a>>() {}
+    pub fn delete_one<'a, T: Deserialize<'a>>(&mut self) {}
 
-    pub fn delete_many<'a, T: Deserialize<'a>>() {}
+    pub fn delete_many<'a, T: Deserialize<'a>>(&mut self) {}
 
-    pub fn count_rows<T: Serialize>() {}
+    pub fn count_rows<T: Serialize>(&mut self) {}
 }
